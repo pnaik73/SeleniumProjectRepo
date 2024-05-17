556a490d7dae99af266ebaeb9a6eacba07a56eb7
Sanpri <Sanpri@LAPTOP-V2EFQHVH.mshome.net>
2024-05-17 10:47:11 +0530

    commit

---
+++ SeleniumProject/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,26 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createcontactTest() {
+		String URL=System.getProperty("url");
+		String browser=System.getProperty("browser");
+		String username=System.getProperty("username");
+		String password=System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
+
+		
+		System.out.println("execute create contactTest");
+	}
+	@Test
+	public void modifycontactTest() {
+		System.out.println("execute  modifycontactTest");
+	}
+	
+
+}
